1ef5b6d5a6e5a5231f577bd0662e6224c3057a1b
mehedi-hasan-dev <mehedi.hasan.rwd@gmail.com>
2024-08-20 16:35:21 +0600

    task

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.banner-wrap').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    speed: 800,
+    pauseOnHover: false,
+});
+
+$('.product-wrap').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: 'left',
+  nextArrow: "right",
+});
